b74a51fd92aef8975ec04f4a7b354856ad5d5330
Anurag Agrawal <anuragagrawal4998@gmail.com>
2025-06-28 15:26:28 +0530

    remove previous files

--- publisher/src/main/java/com/algo/demo/controller/RecursiveFiveWithParentId.java
+++
@@ -1,9 +0,0 @@
-package com.algo.demo.controller;
-
-import java.util.UUID;
-
-@FunctionalInterface
-public interface RecursiveFiveWithParentId <T1, T2, T3, T4, T5, R>{
-    R recurse(RecursiveFiveWithParentId <T1, T2, T3, T4, T5, R> self, T1 param1, T2 param2, T3 param3, T4 param4, T5 param5, int level, UUID parentId, UUID currentId);
-
-}
--- publisher/src/main/java/com/algo/demo/controller/RecursiveThreeWithParentID.java
+++
@@ -1,8 +0,0 @@
-package com.algo.demo.controller;
-
-import java.util.UUID;
-
-public interface RecursiveThreeWithParentID <T1, T2, T3, R> {
-
-    R recurse(RecursiveThreeWithParentID<T1, T2, T3, R> self, T1 param1, T2 param2, T3 param3, int level, UUID parentId, UUID currentId);
-}
